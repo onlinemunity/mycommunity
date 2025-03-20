69ec7ee5efd67d5b9a270005877f1fa18dcd8c58
onlinemunity <onlinemunity@gmail.com>
2025-03-20 08:57:33 +0100

    Update CourseVideo.tsx

--- src/components/courses/CourseVideo.tsx
+++ src/components/courses/CourseVideo.tsx
@@ -27,7 +27,7 @@ export const CourseVideo = ({ course }: CourseVideoProps) => {
             <div className="mb-10">
               <div className="aspect-video relative rounded-md overflow-hidden">
                 <iframe
-                  src={course.video_url}
+                  src={`{course.video_url}`}
                   className="absolute inset-0 w-full h-full"
                   title={`{course.title} preview video`}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
