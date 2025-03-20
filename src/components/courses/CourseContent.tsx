b0df14c98f3939ac6daba43cdf3704bfaf7fb2ae
onlinemunity <onlinemunity@gmail.com>
2025-03-20 08:28:33 +0100

    Update CourseContent.tsx

--- src/components/courses/CourseContent.tsx
+++ src/components/courses/CourseContent.tsx
@@ -95,6 +95,23 @@ export const CourseContent = ({ course }: CourseContentProps) => {
     <Card>
       <CardHeader>
         <CardTitle>Kurs-Übersicht</CardTitle>
+
+        <div className="flex items-center gap-4">
+           {course.video_url && (
+            <div className="mb-10">
+              <div className="aspect-video relative rounded-md overflow-hidden">
+                <iframe
+                  src={course.video_url}
+                  className="absolute inset-0 w-full h-full"
+                  title={`${course.title} preview video`}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  frameBorder="0"
+                  allowFullScreen
+                />
+              </div>
+            </div>
+          )}
+        
       </CardHeader>
       <CardContent>
         {isLoading ? (
